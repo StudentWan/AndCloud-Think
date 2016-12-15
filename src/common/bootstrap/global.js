555d952cb04c20cd2a5d16ab53928b136746a50e
zoolsher <zoolsher@gmail.com>
2016-12-15 23:37:25 +0800

    Fix live compile problem

--- src/common/bootstrap/global.js
+++ src/common/bootstrap/global.js
@@ -5,27 +5,28 @@
 
 /**
  * use global.xxx to define global functions
- * 
+ *
  * global.fn1 = function(){
  *
  * }
  */
-// import ZMQ from 'zmq';
-//
-// think.log("ZMQ Version: "+ZMQ.version,"ZMQ");
-//
-// let sockUrl = think.config('zmq.url');
-// let sock = ZMQ.socket('push');
-//
-// sock.bindSync(sockUrl);
-// think.log("ZMQ binding "+sockUrl,"ZMQ");
-//
-// sock.sendAction = function(actionname,params){
-//     let json = {
-//         type:actionname,
-//         params:params
-//     };
-//     sock.send(JSON.stringify(json));
-// };
-// global.sock = sock;
+import ZMQ from 'zmq';
+if (!global.sock) {
+    think.log("ZMQ Version: " + ZMQ.version, "ZMQ");
 
+    let sockUrl = think.config('zmq.url');
+    let sock = ZMQ.socket('push');
+
+    sock.bindSync(sockUrl);
+    think.log("ZMQ binding " + sockUrl, "ZMQ");
+
+    sock.sendAction = function (actionname, params) {
+        let json = {
+            type: actionname,
+            params: params
+        };
+        sock.send(JSON.stringify(json));
+    };
+    think.log("register global success", "ZMQ");
+    global.sock = sock;
+}
