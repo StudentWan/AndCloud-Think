0e081773896fe7c54e13cf9f18cc0cad0608ab80
Spades-S <allengefeng@163.com>
2017-04-26 14:25:13 +0800

    change calc() to absolute

--- src/home/controller/base.js
+++ src/home/controller/base.js
@@ -4,20 +4,20 @@ export default class extends think.controller.base { //创建controller，继承
     /**
      * some base method in here
      */
-    // async __before() {
-    //     if (this.http.action !== "login") {
-    //         this.user_id = await this.session('user_id');
-    //         if (!think.isEmpty(this.user_id)) {
-    //             let userModel = this.model("users");
-    //             try {
-    //                 this.user = await userModel.getUserViaId(this.user_id);
-    //                 //若已登录，获取用户信息
-    //             } catch (e) {
-    //                 think.log(e.message);
-    //             }
-    //         } else {
-    //             return this.redirect('/user/login');
-    //         }
-    //     }
-    // }
+    async __before() {
+        if (this.http.action !== "login") {
+            this.user_id = await this.session('user_id');
+            if (!think.isEmpty(this.user_id)) {
+                let userModel = this.model("users");
+                try {
+                    this.user = await userModel.getUserViaId(this.user_id);
+                    //若已登录，获取用户信息
+                } catch (e) {
+                    think.log(e.message);
+                }
+            } else {
+                return this.redirect('/user/login');
+            }
+        }
+    }
 }
