0e06035e9197dccf7e3a0d6668bb41047f1c4d30
StudentWan <benyuwan@gmail.com>
2017-06-14 16:52:31 +0800

    change db config

--- src/common/config/db.js
+++ src/common/config/db.js
@@ -16,18 +16,11 @@ export default {
             password: "root", //数据库密码
             prefix: "",
             encoding: "utf8"*/
-            // host: "192.168.8.88",
-            // port: "3306",
-            // database: "andcloud", //数据库名称
-            // user: "root", //数据库帐号
-            // password: "123456", //数据库密码
-            // prefix: "",
-            // encoding: "utf8"
-            host: "127.0.0.1",
+            host: "192.168.8.88",
             port: "3306",
             database: "andcloud", //数据库名称
             user: "root", //数据库帐号
-            password: "203102", //数据库密码
+            password: "123456", //数据库密码
             prefix: "",
             encoding: "utf8"
         },
