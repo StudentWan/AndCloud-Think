6f419d279b3ecaea3ef20f8dd783c9ee4a5ccbac
Spades-S <allengefeng@163.com>
2017-06-19 15:11:58 +0800

    fix ajax in staticreport overflow

--- src/home/model/permissions.js
+++ src/home/model/permissions.js
@@ -1,7 +1,7 @@
 'use strict';
 
 class Permissions extends think.model.base {
-    init (...args) {
+    init(...args) {
         super.init(...args);
         this.tableName = "T_ANDROIDPERMISSIONS"
     }
@@ -9,9 +9,10 @@ class Permissions extends think.model.base {
     async getPermissionInfoByName(permName) {
         let data = await this.alias('permission')
             .field('name as permName, status, info, description')
-            .where({"permission.name": permName}).select();
+            .where({ "permission.name": permName }).select();
         return data;
     }
+
 }
 
 export default Permissions;
