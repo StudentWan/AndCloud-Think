1ada77c84e50d5bfa2f099a386b65c5d97e7f13a
StudentWan <benyuwan@gmail.com>
2017-06-26 12:05:06 +0800

    fix several problem:test time bug/disable button/add testing filename

--- src/home/model/projects.js
+++ src/home/model/projects.js
@@ -27,7 +27,7 @@ class Projects extends think.model.base {
 
     async getProjectByUserID(user_id, perPage, getPage) {
         let data = await this.alias('project')
-            .field('project.id as id, name, project.uploadtime as uploadtime, info.iconimg as logo, label, info.id as infoid')
+            .field('project.id as id, name, project.uploadtime as uploadtime, project.apkfilename as apkfilename, info.iconimg as logo, label, info.id as infoid')
             .join([{
                 table: 'T_APKINFO',
                 as: 'info',
